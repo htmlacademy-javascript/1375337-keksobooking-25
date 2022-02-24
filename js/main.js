2995de64cc8d1352c3c7f96f0f67db59f85d1c5c
LomaevaVV <valyat@yandex.ru>
2022-02-24 23:18:39 +0300

    Добавляет исправления в  функции вывода случайных чисел-2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Доработано на основе: https://dev-gang.ru/article/javascript-generirovat-sluczainoe-czislo-v-diapazone-s1azn94f4k/
 const getRandomNumber = (min, max) => {
   if (min < max && min >= 0) {
-    const RANDOM_NUMBER = Math.random() * ((max + 1) - min) + min;
+    const randomNumber = Math.random() * ((max + 1) - min) + min;
 
-    return Math.floor(RANDOM_NUMBER);
+    return Math.floor(randomNumber);
   } throw new Error('Диапазон задан неверно: минимальное значение диапазона должно быть меньше максимального. Граничные значения диапазона не могут быть меньше 0.');
 };
 
@@ -11,9 +11,9 @@ getRandomNumber(1,5);
 
 const getRandomFloatNumber = (min, max, floatNumber) => {
   if (min < max && min >= 0) {
-    const RANDOM_FLOATNUMBER = Math.random() * ((max) - min) + min;
+    const randomFloatNumber = Math.random() * ((max) - min) + min;
 
-    return Number((RANDOM_FLOATNUMBER).toFixed(floatNumber));
+    return Number((randomFloatNumber).toFixed(floatNumber));
   } throw new Error('Диапазон задан неверно: минимальное значение диапазона должно быть меньше максимального. Граничные значения диапазона не могут быть меньше 0.');
 };
 
