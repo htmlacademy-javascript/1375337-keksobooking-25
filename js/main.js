abf7498e5edb1c289cf2b42fcc7393711043a68d
LomaevaVV <valyat@yandex.ru>
2022-02-24 16:09:23 +0300

    Добавляет исправления в  функции вывода случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 // Доработано на основе: https://dev-gang.ru/article/javascript-generirovat-sluczainoe-czislo-v-diapazone-s1azn94f4k/
-const GET_RANDOM_NUMBER = (min, max) => {
-  if (min > max) {
-    const SWAP = min;
-    min = max;
-    max = SWAP;
-  }
-  const RANDOM_NUMBER = Math.random()*((max+1)-min) + min;
-  return Math.floor(RANDOM_NUMBER);
+const getRandomNumber = (min, max) => {
+  if (min < max && min >= 0) {
+    const RANDOM_NUMBER = Math.random() * ((max + 1) - min) + min;
+
+    return Math.floor(RANDOM_NUMBER);
+  } throw new Error('Диапазон задан неверно: минимальное значение диапазона должно быть меньше максимального. Граничные значения диапазона не могут быть меньше 0.');
 };
 
-GET_RANDOM_NUMBER(1,5);
+getRandomNumber(1,5);
 
-const GET_RANDOM_FLOATNUMBER = (min, max, floatNumber) => {
-  if (min > max) {
-    const SWAP = min;
-    min = max;
-    max = SWAP;
-  }
-  const RANDOM_FLOATNUMBER = Math.random()*((max)-min) + min;
-  return (RANDOM_FLOATNUMBER).toFixed(floatNumber);
+const getRandomFloatNumber = (min, max, floatNumber) => {
+  if (min < max && min >= 0) {
+    const RANDOM_FLOATNUMBER = Math.random() * ((max) - min) + min;
+
+    return Number((RANDOM_FLOATNUMBER).toFixed(floatNumber));
+  } throw new Error('Диапазон задан неверно: минимальное значение диапазона должно быть меньше максимального. Граничные значения диапазона не могут быть меньше 0.');
 };
 
-GET_RANDOM_FLOATNUMBER(1,5,3);
+getRandomFloatNumber(1,5,3);
+
