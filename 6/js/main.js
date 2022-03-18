2de9d233da2b1e3a48546d0ca3ee8e7122dc90a1
Keks <keks@htmlacademy.ru>
2022-03-18 20:57:52 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,12 @@
+import {getAds} from'./data.js';
+import {renderPopupAd} from './card.js';
+import {deactivatePage, activatePage} from './form.js';
+
+const mapCanvas = document.querySelector('#map-canvas');
+
+const ads = getAds();
+const card = renderPopupAd(ads[0]);
+
+mapCanvas.appendChild(card);
+deactivatePage();
+activatePage();
