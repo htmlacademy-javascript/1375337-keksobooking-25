0160f000828424f5fd9ad0bede9d55d5933b21cd
LomaevaVV <valyat@yandex.ru>
2022-03-23 00:46:42 +0300

    Добавляет исправления по ДЗ 8.1-8.2

--- js/form-validation.js
+++ js/form-validation.js
@@ -6,113 +6,109 @@ const TYPE_MIN_PRICE = {
   hotel: 3000
 };
 
-const validateAdForm = (elms) => {
-  const pristine = new Pristine(elms.form, {
-    classTo: 'ad-form__element',
-    errorClass: 'form__item--invalid',
-    successClass: 'form__item--valid',
-    errorTextParent: 'ad-form__element',
-    errorTextTag: 'span',
-    errorTextClass: 'form__error',
-  });
+const MAX_PRICE = 100000;
 
-  //Валидация заголовка
-  const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+const TITLE_SYMBOLS = {
+  min: 30,
+  max: 100
+};
 
-  const getTitleErrorMessage = () => `Текст длиной от 30 до 100 символов. Вы ввели ${elms.title.value.length}.`;
+const adForm = document.querySelector('.ad-form');
+const title = adForm.querySelector('#title');
+const type = adForm.querySelector('#type');
+const price = adForm.querySelector('#price');
+const rooms = adForm.querySelector('#room_number');
+const capacity = adForm.querySelector('#capacity');
+const timein = adForm.querySelector('#timein');
+const timeout = adForm.querySelector('#timeout');
 
+const createPristineInstance = () => new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error',
+});
+
+//Валидация заголовка
+const validateTitle = (value) => value.length >= TITLE_SYMBOLS.min && value.length <= TITLE_SYMBOLS.max;
+const getTitleErrorMessage = () => `Текст длиной от ${TITLE_SYMBOLS.min} до ${TITLE_SYMBOLS.max} символов. Вы ввели ${title.value.length}.`;
+
+//Валидация цены
+const validatePrice = (value) => Number.isInteger(Number(value)) && value >= TYPE_MIN_PRICE[type.value] && value <= MAX_PRICE;
+const getPriceErrorMessage = () => `Цена: от ${TYPE_MIN_PRICE[type.value]} до ${MAX_PRICE}`;
+
+// Валидация времени Заезда/выезда
+const changeTime = (timeSelected, timeArToChange) =>
+  Array.from(timeArToChange).forEach((element) => element.value === timeSelected ? (element.selected = true) : false);
+const onTimeinChange = (evt) => changeTime(evt.target.value, timein);
+const onTimeoutChange = (evt) => changeTime(evt.target.value, timeout);
+
+//Изменение мин. цены в зависимости от типа жилья
+const onPriceChange = (evt) => (price.placeholder = TYPE_MIN_PRICE[evt.target.value]);
+
+
+// Валидация Комнат и гостей
+const validateRoomsAndCapacity = () => {
+  const roomsCount = parseInt(rooms.value, 10);
+  const capacityCount = parseInt(capacity.value, 10);
+
+  return roomsCount === 100
+    ? capacityCount === 0
+    : capacityCount > 0 && roomsCount >= capacityCount;
+};
+
+const onFormSubmit = (evt, pristine) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+const onFormChange = (evt, pristine) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+// Добавляет Валидаторы
+const addValidators = (pristine) => {
   pristine.addValidator(
-    elms.title,
+    title,
     validateTitle,
     getTitleErrorMessage
   );
 
-  //Валидация цены
-  const validatePrice = (value) => {
-    const selectedType = elms.type.querySelector('option:checked');
-
-    return value % 1 === 0 && value >= TYPE_MIN_PRICE[selectedType.value] && value <= 100000;
-  };
-
-  const getPriceErrorMessage = () => {
-    const selectedType = elms.type.querySelector('option:checked');
-
-    return `Цена: от ${TYPE_MIN_PRICE[selectedType.value]} до 100 000`;
-  };
-
   pristine.addValidator(
-    elms.price,
+    price,
     validatePrice,
     getPriceErrorMessage
   );
 
-  //Изменение мин. цены в зависимости от типа жилья
-  const changeMinPrice = () => {
-    const selectedType = elms.type.querySelector('option:checked');
-    elms.price.placeholder = TYPE_MIN_PRICE[selectedType.value];
-  };
-
-  elms.type.addEventListener('change', changeMinPrice);
-
-  // Валидация Комнат и гостей
-  const validateRoomsAndCapacity = () => {
-    const roomsCount = parseInt(elms.rooms.value, 10);
-    const capacityCount = parseInt(elms.capacity.value, 10);
-
-    return roomsCount === 100
-      ? capacityCount === 0
-      : capacityCount > 0 && roomsCount >= capacityCount;
-  };
-
   pristine.addValidator(
-    elms.rooms,
+    rooms,
     validateRoomsAndCapacity,
     'Комнат должно быть >= гостей. 100 комнат - "не для гостей"'
   );
 
   pristine.addValidator(
-    elms.capacity,
+    capacity,
     validateRoomsAndCapacity,
     'Гостей должно быть <= комнат'
   );
 
-  // Валидация времени Заезда/выезда
-  const changeTime = (timeArSelected, timeArToChange) => {
-    const selectedTime = timeArSelected.querySelector('option:checked');
-
-    for (let i = 0; i < timeArToChange.length; i++) {
-      if (timeArToChange[i].value === selectedTime.value) {
-        timeArToChange[i].selected = true;
-      }
-    }
-  };
-
-  const changeTimeIn = () => {
-    changeTime(elms.timeout, elms.timein);
-  };
-
-  const changeTimeOut = () => {
-    changeTime(elms.timein, elms.timeout);
-  };
-
-  elms.timeout.addEventListener('change', changeTimeIn);
-
-  elms.timein.addEventListener('change', changeTimeOut);
-
-  // Обработчики
-  elms.form.addEventListener('change', (evt) => {
-    evt.preventDefault();
-    pristine.validate();
-  });
-
-  elms.form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    pristine.validate();
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    pristine.validate();
-  });
 };
 
-export {validateAdForm};
+//Запускает обработчики
+const setFormListeners = () => {
+  const pristine = createPristineInstance();
+
+  timeout.addEventListener('change', onTimeinChange);
+  timein.addEventListener('change', onTimeoutChange);
+  type.addEventListener('change', onPriceChange);
+
+  adForm.addEventListener('submit', (evt) => onFormSubmit(evt, pristine));
+  adForm.addEventListener('change', (evt) => onFormChange(evt, pristine));
+
+  addValidators(pristine);
+};
+
+export {setFormListeners};
--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
 import {getAds} from'./data.js';
 import {renderPopupAd} from './card.js';
 import {deactivatePage, activatePage} from './form.js';
-import {validateAdForm} from './form-validation.js';
+import {setFormListeners} from './form-validation.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 
 const ads = getAds();
-const adForm = document.querySelector('.ad-form');
-const adFormElements = {
-  form: adForm,
-  title: adForm.querySelector('#title'),
-  type: adForm.querySelector('#type'),
-  price: adForm.querySelector('#price'),
-  rooms: adForm.querySelector('#room_number'),
-  capacity: adForm.querySelector('#capacity'),
-  timein: adForm.querySelector('#timein'),
-  timeout: adForm.querySelector('#timeout')
-};
 
 const card = renderPopupAd(ads[0]);
 mapCanvas.appendChild(card);
@@ -25,4 +14,4 @@ deactivatePage();
 
 activatePage();
 
-validateAdForm(adFormElements);
+setFormListeners();
